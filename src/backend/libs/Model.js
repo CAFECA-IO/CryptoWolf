2edbf0db70e21d4d0287839c19939fc71502ae8d
wayne.lee <wayne.lee@tideisun.com>
2021-11-08 16:09:31 +0800

    fix eslint

--- src/backend/libs/Model.js
+++ src/backend/libs/Model.js
@@ -1,7 +1,9 @@
 class Model {
+  // eslint-disable-next-line no-unused-vars
   constructor({ condition, query }) {
     return Promise.resolve(this);
   }
+
   save() {
     return Promise.resolve(this);
   }
